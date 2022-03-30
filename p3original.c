cd2e9d967480ea5724516e7b294cc62dcc331f25
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-03-30 20:10:55 +0000

    assignment

--- p3original.c
+++ p3original.c
@@ -1,36 +1,38 @@
 #include<stdio.h>
-#include<math.h>
+
 int input_number()
 {
-  int num;
+  int n;
   printf("enter a number\n");
-  scanf("%d\n",&num);
+  scanf("%d\n",&n);
 }
 
 int is_prime(int n)
 {
-  int sum=0;
-  if(int i=2;i<n;i++)
+  for(int i=1;i<=n;i++)
   {
-    if((n%i)==0)
-      sum++;
+    if(n%i==0) ;  
   }
-  return sum;
+  return 0;
 }
-void output(float x1,float y1,float x2,float y2,float x3,float y3)
+void output(int n,int is_prime)
 {
   if(is_prime==0)
+  {
     printf("%d is a prime number\n",n);
+  }
   else
+  {
     printf("%d is not a prime number\n",n);
+  }
 }
 
 int main()
 {
-  input n,cmp;
+  int n,c;
   n=input_number();
-  cmp=is_prime();
-  output(n,cmp);
+  c=is_prime(n);
+  output(n,c);
   return 0;
   
 }
