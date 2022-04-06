297b55189c23fa0a7313dbec40bd724327c0b1e6
Soumya Dhanappa <97490356+Soumya5122@users.noreply.github.com>
2022-04-06 17:29:36 +0000

    main

--- p2original.c
+++ p2original.c
@@ -2,7 +2,7 @@
 #include<math.h>
 void input_triangle(float *x1,float *y1,float *x2,float *y2,float *x3,float *y3)
 {
-   printf("enter the six numbers\n);
+   printf("enter the six numbers");
   scanf("%f%f%f%f%f%f",x1,y1,x2,y2,x3,y3);
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
@@ -29,9 +29,7 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3, int istri
 int main()
 {
   float x1,x2,x3,y1,y2,y3;
-  input_line(&x1,&y1);
-  input_line(&x2,&y2);
-  input_line(&x3,&y3);
+  input_triangle(&x1,&y1,&x2,&y2,&x3,&y3);
   int a=is_triangle(x1,y1,x2,y2,x3,y3);
   output(x1,y1,x2,y2,x3,y3,a);
   return 0;
